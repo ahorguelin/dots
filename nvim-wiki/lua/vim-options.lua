03bcf08c4456f91d3423d615462967c1a7eded32
ahorguelin <antoinehorguelin@gmail.com>
2024-05-19 08:40:39 +0200

    added minimalist nvim config for md note taking

---
+++ nvim-wiki/lua/vim-options.lua
@@ -0,0 +1,37 @@
+vim.g.mapleader = " "
+
+vim.cmd("set expandtab") 
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set scrolloff=8")
+
+vim.cmd("set number relativenumber")
+
+vim.cmd("set syntax")
+vim.cmd("set nocompatible")
+
+vim.cmd("set incsearch")
+vim.cmd("set ignorecase")
+vim.cmd("set smartcase")
+vim.cmd("set showmode")
+
+vim.cmd("set noswapfile")
+
+vim.keymap.set("n", "<leader>E", vim.cmd.Ex)
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "{", "{zz")
+vim.keymap.set("n", "}", "}zz")
+
+
+
+vim.keymap.set("n", "<leader>v", "<C-w>v")
+vim.keymap.set("n", "<leader>s", "<C-w>s")
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
+vim.keymap.set({"n","v" }, "<leader>P", "\"+p")
+vim.keymap.set("v", "<leader>Y", "\"+y")
